634ac684968164e2a1ef252a4e9b1970fc0632ff
Montyer930 <montyer930@gmail.com>
2023-05-30 16:38:52 -0500

    tercer commit

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ async def predict(input_data: PredictionInput):
 
 # Punto de entrada del servidor de desarrollo de FastAPI
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
